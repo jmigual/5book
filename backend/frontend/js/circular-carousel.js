ab5278e2091dd0e5f02c834a2db076a9230d20c0
jmigual <j.marce.igual@gmail.com>
2017-02-20 17:52:23 +0100

    Improved carousel

--- backend/frontend/js/circular-carousel.js
+++ backend/frontend/js/circular-carousel.js
@@ -2,25 +2,62 @@
  * Created by joan on 20/02/17.
  */
 
-$(document).ready(function () {
-  $(window).resize(function () {
-    $(".carousel").each(drawCarousel);
-  });
-  $(".carousel").each(drawCarousel);
-});
-
-function drawCarousel() {
-  var childs = $(this).children();
-  if (childs.length <= 0) 
-    return;
+(function ($) {
   
-  var count = childs.length;
-  var width = $(this).width();
-  var angleStep = (2*Math.PI)/(childs.length);
-  var radius = Math.round((width/2)/Math.tan(angleStep/2));
-  console.log("Width:", width, "Angle Step:", angleStep, "Radius:", radius);
+  $.fn.CircularCarousel = function (options) {
   
-  var selected = $(this).data("selected");
-  var theta = (2*Math.PI)/count*(selected);
-  $(this).css("transform", "translateZ(" + (-radius) + "px) rotateY(" + theta + ")");
-}
+    var $ele = $(this);
+    var childs = $ele.children("figure"),
+        count = childs.length,
+        angleStep = (2*Math.PI)/count,
+        width = $ele.width(),
+        radius = Math.round((width/2)/Math.tan(angleStep/2));
+        activeItem = options.activeItem;
+  
+    console.log(childs);
+    console.log("Width:", width, "Angle Step:", angleStep, "Radius:", radius);
+  
+    function drawCarousel() {
+      if (count <= 0)
+        return;
+  
+      width = $ele.width();
+      radius = Math.round(width/2) - 1;
+      var imageWidth = 2*radius*Math.cos(angleStep/2);
+      
+      $(childs).each(function(i) {
+        var thetaInterior = (360/count)*i;
+        console.log(this);
+        $(this).css({"transform": "rotateY(" + thetaInterior + "deg) translateZ(" + radius + "px)" });
+        $(this).width(imageWidth);
+      });
+  
+      $(this).height($(childs[0]).height() + 50);
+    }
+    
+    function updateCarousel() {
+      if (count <= 0)
+        return;
+    
+      var theta = 360/count*(activeItem);
+      $ele.css("transform", "translateZ(" + (-radius) + "px) rotateY(" + theta + "deg)");
+    }
+    
+    console.log("Pepito");
+    
+    drawCarousel();
+    updateCarousel();
+    
+    $(document).ready(function() {
+      $(window).resize(drawCarousel);
+    });
+    
+    var methods = {
+      cycleActive: function (direction) {
+        activeItem += ((direction === 'previous')) ? -1 : 1;
+        updateCarousel();
+      }
+    }
+  };
+  
+}(jQuery));
