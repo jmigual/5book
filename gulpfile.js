a78f52d2bac468e62a0cd33b4d3ca6738d30f831
jmigual <j.marce.igual@gmail.com>
2017-02-28 13:38:01 +0100

    Added more functions to gulp compilation

--- gulpfile.js
+++ gulpfile.js
@@ -1,80 +1,117 @@
 "use strict";
-var gulp = require('gulp');
-var gutil = require('gulp-util');
-var pump = require('pump');
-var uglify = require('gulp-uglify');
-var csso = require('gulp-csso');
-var inject = require('gulp-inject');
+var gulp           = require('gulp');
+var gutil          = require('gulp-util');
+var pump           = require('pump');
+var uglify         = require('gulp-uglify');
+var csso           = require('gulp-csso');
+var inject         = require('gulp-inject');
 var mainBowerFiles = require('main-bower-files');
-var clean = require('gulp-clean');
-var gulpCopy = require('gulp-copy');
+var clean          = require('gulp-clean');
+var gulpCopy       = require('gulp-copy');
+var changed        = require('gulp-changed');
+var runSequence    = require('run-sequence');
+var htmlmin        = require('gulp-htmlmin');
+var argv           = require('yargs').argv;
 
 var destFolder = 'build/';
 var destPublic = destFolder + 'public/';
-var debug = false;
+var debug      = false;
 
-var extraFiles = [__dirname + '/public/bower_components/fullpage.js/vendors/scrolloverflow.min.js'];
+function fileChanged(stream, callback, sourceFile, destPath) {
+    return changed.compareLastModifiedTime(stream, callback, sourceFile, destPath);
+}
 
-var bowerFiles = mainBowerFiles({
-    paths: 'public'
-});
-var allBowerFiles = bowerFiles.concat(extraFiles);
+function getBowerFiles() {
+    return mainBowerFiles({
+        paths    : 'public',
+        overrides: {
+            'fullpage.js': {
+                main: [
+                    debug ? 'vendors/scrolloverflow.min.js' : 'vendors/scrolloverflow.js',
+                    debug ? 'dist/jquery.fullpage.min.js' : 'dist/jquery.fullpage.js',
+                    debug ? 'dist/jquery.fullpage.min.css' : 'dist/jquery.fullpage.css'
+                ]
+            }
+        }
+    });
+}
 
 gulp.task('css', function (cb) {
+    var destCSS = destPublic + 'css';
+    
     pump([
         gulp.src('public/css/*.css'),
+        changed(destCSS),
         csso({
             restructure: !debug,
-            sourceMap: debug,
-            debug: debug
+            sourceMap  : debug
         }),
-        gulp.dest(destPublic + 'css')
+        gulp.dest(destCSS)
     ], cb);
 });
 
-gulp.task('scripts', function(cb) {
+gulp.task('scripts', function (cb) {
+    var destJs = destPublic + 'js';
+    
     pump([
         gulp.src('public/js/*.js'),
+        changed(destJs),
         uglify({
             preserveComments: 'license'
         }),
-        gulp.dest(destPublic + 'js')
+        gulp.dest(destJs)
     ], cb);
 });
 
-gulp.task('html', function(cb) {
-    console.log(allBowerFiles);
+gulp.task('html', function (cb) {
     pump([
         gulp.src('public/*.html'),
-        inject(gulp.src(allBowerFiles, { read: false }), 
-            { 
-                name: 'bower',
+        changed(destPublic, { hasChanged: fileChanged }),
+        inject(gulp.src(getBowerFiles(), { read: false }),
+            {
+                name    : 'bower',
                 relative: true
             }),
+        htmlmin({
+            collapseWhitespace: true
+        }),
         gulp.dest(destPublic)
     ], cb);
 });
 
 gulp.task('libraries', function (cb) {
+    var destLib = destPublic + 'bower_components';
+    
     pump([
-        gulp.src(allBowerFiles),
-        gulpCopy(destPublic + 'bower_components', { prefix: 2})
+        gulp.src(getBowerFiles(), { read: false }),
+        changed(destLib),
+        gulpCopy(destLib, { prefix: 2 })
     ], cb);
 });
 
 gulp.task('images', function (cb) {
+    var destImg = destPublic + 'img';
+    
     pump([
-        gulp.src('public/img/*'),
-        gulp.dest(destPublic + 'img')
-    ]);
+        gulp.src('public/img/*', { read: false }),
+        changed(destImg),
+        gulp.dest(destImg)
+    ], cb);
 });
 
-gulp.task('default', ['css', 'scripts', 'html', 'libraries', 'images'], function () {
-    debug = debug || false;
+gulp.task('clean', function (cb) {
+    pump([
+        gulp.src(destFolder, { read: false }),
+        clean()
+    ], cb);
 });
 
-gulp.task('debug', function() {
-    debug = true;
-    gutil.log(gutil.colors.green('RUNNING IN DEBUG MODE'));
-    gulp.start('default');
+gulp.task('rebuild', function () {
+    runSequence('clean', 'default');
+});
+
+gulp.task('default', function () {
+    debug = argv.type === 'debug';
+    gutil.log(gutil.colors.green('Running in ' + (debug ? 'debug' : 'release') + ' mode'));
+    gulp.start(['css', 'scripts', 'html', 'libraries', 'images']);
 });
