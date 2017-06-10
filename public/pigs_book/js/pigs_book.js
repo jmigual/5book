4861682725dd40eee6cee74cea53494dc9a74fea
jmigual <j.marce.igual@gmail.com>
2017-06-10 14:44:08 +0200

    Add first video

--- public/pigs_book/js/pigs_book.js
+++ public/pigs_book/js/pigs_book.js
@@ -1,8 +1,9 @@
-const $      = require('jquery');
-const jQuery = $;
-const PIXI   = require('pixi.js');
-const p2     = require('p2');
-const marked = require('marked');
+const $       = require('jquery');
+const jQuery  = $;
+const PIXI    = require('pixi.js');
+const p2      = require('p2');
+const marked  = require('marked');
+const videojs = require('video.js');
 require('./game');
 require('./jquery.fullpage');
 require('./sketch');
@@ -13,21 +14,21 @@ window.requestAnimationFrame = (function () {
         window.webkitRequestAnimationFrame ||
         window.msRequestAnimationFrame ||
         function (callback) {
-            window.setTimeout(callback, 1000/30);
+            window.setTimeout(callback, 1000 / 30);
         };
 })();
 
 $(document).ready(function () {
     $("#pong-game-canvas").PongGame();
-    
+
     $(".markdown").each(function () {
         $(this).html(marked($(this).text()));
     });
-    
+
     const elem = $(".canvas-view");
     elem.sketch();
     elem.sketch().redraw();
-    
+
     $("#main-container").fullpage({
         verticalCentered: true,
         loopBottom      : false,
@@ -37,22 +38,22 @@ $(document).ready(function () {
             console.log(anchorLink, index);
         }
     });
-    
+
     $("#buttonNext").click(() => {
         $.fn.fullpage.moveSectionDown();
     });
-    
+
     $("#buttonPrev").click(() => {
         $.fn.fullpage.moveSectionUp();
     });
-    
-    $("#buttonSound").click( function() {
+
+    $("#buttonSound").click(function () {
         let active = $(this).data("active");
         this.src   = "img/rendered/" + (active ? "sound_off.png" : "sound.png");
         $(this).data("active", !active);
     });
-    
-    $("#buttonStart").click(function() {
+
+    $("#buttonStart").click(function () {
         $.fn.fullpage.moveTo(1);
     })
 });
