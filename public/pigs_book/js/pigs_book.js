16159c74a2187bc4fc060efb5db7cab84bcdbdd0
jmigual <j.marce.igual@gmail.com>
2017-06-12 11:40:12 +0200

    Add audio to the web

--- public/pigs_book/js/pigs_book.js
+++ public/pigs_book/js/pigs_book.js
@@ -1,9 +1,10 @@
-const $       = require('jquery');
-const jQuery  = $;
-const PIXI    = require('pixi.js');
-const p2      = require('p2');
-const marked  = require('marked');
-const videojs = require('video.js');
+const $      = require('jquery');
+const jQuery = $;
+const PIXI   = require('pixi.js');
+const p2     = require('p2');
+const marked = require('marked');
+const video  = require('video.js');
+const howler = require('howler');
 require('./game');
 require('./jquery.fullpage');
 require('./sketch');
@@ -14,21 +15,21 @@ window.requestAnimationFrame = (function () {
         window.webkitRequestAnimationFrame ||
         window.msRequestAnimationFrame ||
         function (callback) {
-            window.setTimeout(callback, 1000 / 30);
+            window.setTimeout(callback, 1000/30);
         };
 })();
 
 $(document).ready(function () {
     $("#pong-game-canvas").PongGame();
-
+    
     $(".markdown").each(function () {
         $(this).html(marked($(this).text()));
     });
-
+    
     const elem = $(".canvas-view");
     elem.sketch();
     elem.sketch().redraw();
-
+    
     $("#main-container").fullpage({
         verticalCentered: true,
         loopBottom      : false,
@@ -36,27 +37,58 @@ $(document).ready(function () {
         navigation      : true,
         paddingBottom   : "100px",
         fixedElements   : "#background",
-        afterLoad       : function (anchorLink, index) {
-            console.log(anchorLink, index);
-        }
+        afterLoad       : onLoad,
+        onLeave         : onLeave
     });
-
+    
     $("#buttonNext").click(() => {
         $.fn.fullpage.moveSectionDown();
     });
-
+    
     $("#buttonPrev").click(() => {
         $.fn.fullpage.moveSectionUp();
     });
-
-    $("#buttonSound").click(function () {
-        let active = $(this).data("active");
-        this.src   = "img/rendered/" + (active ? "sound_off.png" : "sound.png");
-        $(this).data("active", !active);
-    });
-
+    
     $("#buttonStart").click(function () {
         $.fn.fullpage.moveTo(1);
-    })
+    });
+    
+    let active = true;
+    let sounds = {};
+    
+    $("#buttonSound").click(function () {
+        active   = $(this).data("active");
+        this.src = "img/rendered/" + (active ? "sound.png" : "sound_off.png");
+        $(this).data("active", !active);
+        
+        howler.Howler.mute(!active);
+    });
+    
+    ['challenge', 'game', 'text', 'draw'].map(elem => {
+        sounds[elem] = new howler.Howl({
+            src   : [`audio/${elem}.mp3`],
+            loop  : true,
+            onfade: function () {
+                if (this.volume() === 0) this.pause();
+            }
+        });
+    });
+    
+    function onLoad() {
+        let type = $(this).data('type');
+        if (!active || !type) return;
+        
+        console.log(type);
+        sounds[type].play();
+        sounds[type].fade(0, 1, 500);
+    }
+    
+    function onLeave() {
+        let type = $(this).data('type');
+        if (!active || !type) return;
+        
+        console.log("Leaving: ", type);
+        sounds[type].fade(1, 0, 500);
+    }
 });
 
